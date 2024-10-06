6210cb7c67b0c6246195903ac8bde37953ddfacf
Deyen28 <132223571+Deyen28@users.noreply.github.com>
2024-10-06 13:11:56 -0500

    Add files via upload

---
+++ proyectoAulaBD/src/main/java/com/proyectoAulaBD/ProyectoAulaBdApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoAulaBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAulaBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAulaBdApplication.class, args);
+	}
+
+}
